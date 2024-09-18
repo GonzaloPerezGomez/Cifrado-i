0d5177d0b0021e7f111c82c978648fb14d8256b1
LSI <gonzaloperezgo@gmail.com>
2024-09-18 10:57:33 +0200

    seguridad

---
+++ analisis_frecuencia.py
@@ -0,0 +1,20 @@
+
+def descifrar(msg):
+
+	frecuencias = [[],[]]
+	
+	for i in frecuencias:
+		frecuencias[i][0] = 
+	
+	
+	
+
+
+
+def main():
+
+	mensaje = input(f"Introduce el mensaje a cescifrar: ")
+	descifrar(mensaje)
+	
+
+
