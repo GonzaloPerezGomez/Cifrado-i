7a5c65a3e21fed0ea1a50299d6ff66d0bed8bfbf
GonzaloP <gonzaloperezgo@gmail.com>
2024-09-21 12:16:46 +0200

    Update analisis_frecuencia.py

--- analisis_frecuencia.py
+++ analisis_frecuencia.py
@@ -1,15 +1,14 @@
 
 def descifrar(msg):
 	str(msg)
-	frecuencias = {}	
+	frecuencias = {}
+	dicc = ["e","a","o","l","s","n","d","r","u","i","t","c","p","m","y","q","b","h","g","f","v","j","ñ","z","x","k","w"]
 	
 	for letra in msg:
 		if letra.isalpha():
-			if letra in frecuencias:
-				frecuencias[letra] += 1
-			else:
-				frecuencias[letra] = 1
+			frecuencias[letra] = frecuencias.get(letra, 0) + 1
 
+	frecuencias = dict(sorted(frecuencias.items(), key=lambda item: item[1], reverse=True))
 	print(frecuencias)
 
 
